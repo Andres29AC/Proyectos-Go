7a4bc52cdb33dc073a770f2f4f176a8a5e8bd4c4
Andres Amaro <lionAS_45@hotmail.com>
2023-12-13 03:55:28 -0500

    Bot terminado para slack

--- src/Slack-Age-Bot/go.mod
+++ src/Slack-Age-Bot/go.mod
@@ -1,3 +1,16 @@
 module github.com/Andres29AC/Proyectos-Go/src/Slack-Age-Bot
 
 go 1.19
+
+require github.com/slack-io/slacker v0.1.0
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/shomali11/commander v0.0.0-20230730023802-0b64f620037d // indirect
+	github.com/shomali11/proper v0.0.0-20190608032528-6e70a05688e7 // indirect
+	github.com/shomali11/slacker v1.4.1 // indirect
+	github.com/slack-go/slack v0.12.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ src/Slack-Age-Bot/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Evenets")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-5474558710455-6349869728673-kM70pM1UFzlXl81TIPJMuB2z")
+	os.Setenv("SLACK_APP_TOKEN","xapp-1-A06AKUUV5UY-6349834150817-cbe3d8f18bcff96a7db7b34101ad1c7e2d32fa41055bbb3cb895860fec69a182")
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator. Example: 'my yob is 2020'",	
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2021 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
