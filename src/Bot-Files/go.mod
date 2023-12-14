8b86962b3d60dc4c049e2ed5ca31fcf1175f3968
Andres Amaro <lionAS_45@hotmail.com>
2023-12-13 23:34:14 -0500

    Primera version del bot para Slack

---
+++ src/Bot-Files/go.mod
@@ -0,0 +1,10 @@
+module github.com/Andres29AC/Proyectos-Go/src/Bot-Files
+
+go 1.19
+
+require github.com/slack-go/slack v0.12.3
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+)
--- src/Bot-Files/main.go
+++ src/Bot-Files/main.go
@@ -1,1 +1,29 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-5474558710455-6328726628551-34mrG1i7qk84P95cfF1kZk0Z")
+	//NOTE: CHANNEL_IO en este caso es el id del canal donde se quiere subir el archivo
+	os.Setenv("CHANNEL_IO","C05ED2U81K4")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_IO")}
+	fileArr := []string{"RSL.pdf"}
+
+	for i := 0; i < len(channelArr); i++{
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return 
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+}
